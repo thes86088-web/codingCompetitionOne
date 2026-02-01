fc26ec8da1c3e58c54e4341999a4973072f12261
durvesh <thes86088@gmail.com>
2026-02-01 12:09:29 +0530

    define the required variables and function

--- R2Q2_costliestProductFromArray.c
+++ R2Q2_costliestProductFromArray.c
@@ -1,9 +1,17 @@
-/*
 
-input provided : 
-int prices[] or int *prices
 
-output expected : 
-price of the costliest product in the given array
+int prices[] = { 3, 6, 11, 5, 8, 6, 33, 21};
+int len = (sizeof(prices))/(sizeof(int)) ;
 
-*/
+int linearSearch( int prices[], int len )
+{
+  
+}
+
+int main()
+{
+  int index = linearSearch( prices, len );
+  printf("costliest product is present at index = %d", index ) ;
+}
+
+
