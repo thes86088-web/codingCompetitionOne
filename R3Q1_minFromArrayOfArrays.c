96857ce562e86682f2eb1bf4f0624c792646c5f1
durvesh <thes86088@gmail.com>
2026-02-01 13:34:58 +0530

    no errors, but still bugs

--- R3Q1_minFromArrayOfArrays.c
+++ R3Q1_minFromArrayOfArrays.c
@@ -38,20 +38,13 @@ int searchByHalves( int unfolded[], int low, int high, int default_min )
 	if( low<high && low>=0 )
 	{
 		int half = ( low + high )/2;
-		if( unfolded[half] < unfolded[default_min] && default_min>=0 )
-		{
-			default_min = half;
-			return half;
-		}
-		else
-		{
+
 			int left = searchByHalves(unfolded, low, half, default_min );
 			int right = searchByHalves( unfolded, half+1, high, default_min );
-			if( unfolded[left]<unfolded[right] && left>=0  ) return left;
-			else if( unfolded[left]>unfolded[right] && right>=0  ) return right;
-		}
-
+			if( unfolded[left]<unfolded[right] && left>=0 && left<=right  ) return left;
+			else if( unfolded[left]>unfolded[right] && right>=low  ) return right;
 	}
+
 	return -1;
 }
 
@@ -59,7 +52,7 @@ int searchByHalves( int unfolded[], int low, int high, int default_min )
 int main()
 {
 // int *marksList = unfold(marks);
-	int marksList[] = {  2,5,6,11,3,4,5,9,4,2, 13, 5, 1, 8, 7 };
+	int marksList[] = {  2,5,6,11,3,4,5,9,4,2, 13, 5, 11, 8, 7 };
 	int min_marks = searchByHalves( marksList, 0, len-1, marksList[0] );
 	printf( "lowest marks obtained = %d", marksList[min_marks] );
 	printf("\n");
