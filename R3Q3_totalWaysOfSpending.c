f71e61eae81d4742827ed42ddb672cc56977cdaf
durvesh <thes86088@gmail.com>
2026-02-01 10:33:43 +0530

    add the code body for required function

--- R3Q3_totalWaysOfSpending.c
+++ R3Q3_totalWaysOfSpending.c
@@ -5,16 +5,25 @@ int amount = 20 ;
 int prices[] = {20, 10, 5} ;
 int items = ( sizeof(prices) )/( sizeof(*prices) ) ;
 
-int validSpending( int amount, int prices, int items )
+int validSpending( int amount, int price[], int items )
 {
   if( amount < 0 ) return 0;
 
-  else if( amount == 0 ) return 0;
+  else if( amount == 0 ) return 1;
+
+  else
+  {
+    int way0 = validSpending( amount-prices[0], prices, items ) ;
+    int way1 = validSpending( amount-prices[1], prices, items ) ;
+    int way2 = validSpending( amount-prices[2], prices, items ) ;
+    
+    return (way0+way1+way2) ;
+  }
 
 }
 
 int main()
 {
     int ways = validSpending( amount, prices, items );
-    printf( "total valid ways of spending are : %d " );
+    printf( "total valid ways of spending are : %d ", ways );
 }
