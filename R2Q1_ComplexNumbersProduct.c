c616a21b382703a03c811fad8a5c1b7fc3346659
durvesh <thes86088@gmail.com>
2026-02-01 10:18:04 +0530

    Create R2Q1_ComplexNumbersProduct.c

---
+++ R2Q1_ComplexNumbersProduct.c
@@ -0,0 +1,12 @@
+/*
+
+input provided : complex numbers a, b
+
+output expected :
+i. product of these numbers ( a*b )
+ii. quotient of these nemubers ( a/b ) 
+
+approach :
+define struct complex
+
+*/
