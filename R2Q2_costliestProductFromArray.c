2b24b4818ed18d594dda42ff0cb10255ded37127
durvesh <thes86088@gmail.com>
2026-02-01 12:14:43 +0530

    add function body for linearSearch

--- R2Q2_costliestProductFromArray.c
+++ R2Q2_costliestProductFromArray.c
@@ -1,17 +1,26 @@
 
+#include <stdio.h>
 
 int prices[] = { 3, 6, 11, 5, 8, 6, 33, 21};
 int len = (sizeof(prices))/(sizeof(int)) ;
 
 int linearSearch( int prices[], int len )
 {
-  
+  int max = 0;
+  for( int i=0; i<len; i++ )
+  {
+    if( prices[i]>prices[max] )
+    {
+      max = i;
+    }
+  }
+  return max;
 }
 
 int main()
 {
   int index = linearSearch( prices, len );
-  printf("costliest product is present at index = %d", index ) ;
+  printf("costliest product is %d present at index = %d", prices[index], index ) ;
 }
 
 
