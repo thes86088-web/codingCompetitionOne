48bf2b98fe2aa006e4ec60c4d8b98676aac8bd31
durvesh <thes86088@gmail.com>
2026-02-01 12:36:35 +0530

    final version - R3Q1Apr1

--- R3Q1_minFromArrayOfArrays.c
+++ R3Q1_minFromArrayOfArrays.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /*
 Approach-1 : Searching using Nested Loops
 */
@@ -10,24 +11,28 @@ Approach-1 : Searching using Nested Loops
  { 13, 5, 1, 8, 7},
  };
 
-int *lowestMarks( marks )
+int *lowestMarks( int marks[][batchSize] )
 {
   int *min_info = calloc( sizeof(int), 2 );
-   
+  int min_batch = 0 ; int min_roll = 0;
   for( int i = 0; i<batches; i++ )
   {
-      for( int i = 0; i<batches; i++ )
+      for( int j = 0; j<batches; j++ )
     {
-        /*code body*/    
+        if( marks[i][j] < marks[min_batch][min_roll]  )
+        {
+          min_batch = i; min_roll = j;
+        }
     }
 
   }
+  min_info[0] = min_batch; min_info[1] = min_roll;
   return min_info;
 }
 
 int main()
 {
   int *min_info = lowestMarks( marks );
-  printf( "lowest marks are obtained in batch-%d by roll-%d : ", min_info[0], min_info[1] );
+  printf( "lowest marks are obtained in batch-%d by roll-%d : %d", min_info[0], min_info[1], marks[ min_info[0] ][ min_info[1] ]  );
   printf("\n");
 }
