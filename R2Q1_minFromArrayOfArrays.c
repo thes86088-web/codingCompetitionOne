68b15103baa58cb03fdf7e0278d106a22ced9d90
durvesh <thes86088@gmail.com>
2026-02-01 10:10:05 +0530

    Create R2Q1_minFromArrayOfArrays.c

---
+++ R2Q1_minFromArrayOfArrays.c
@@ -0,0 +1,24 @@
+/*
+Input Provided :
+int arr[][] or int **arr
+(positive integers)
+
+Output Expected :
+the integer with minimum value from the given 2D array
+*/
+
+/*
+Approach-1 : Searching using Nested Loops
+*/
+
+
+/*
+Approach-2 : Searching by dividing into Halves
+constraint: int* is preferrable over int[][]
+dis_adv : recursive approach
+*/
+
+/*
+Approach-3 : Unfolding ( int arr[][] --> int *arr ) and performing linear search 
+dis_adv :  : has to visit every element twice ( once for unfolding and once for searching )
+*/
