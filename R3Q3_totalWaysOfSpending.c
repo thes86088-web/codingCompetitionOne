48aa044c581e1b5bfa7b6f3229353c6060a5a950
durvesh <thes86088@gmail.com>
2026-02-01 10:40:12 +0530

    add repeating recursive steps inside a loop

--- R3Q3_totalWaysOfSpending.c
+++ R3Q3_totalWaysOfSpending.c
@@ -5,25 +5,27 @@ int amount = 20 ;
 int prices[] = {20, 10, 5} ;
 int items = ( sizeof(prices) )/( sizeof(*prices) ) ;
 
-int validSpending( int amount, int price[], int items )
+int validSpending( int amount, int prices[], int items )
 {
-  if( amount < 0 ) return 0;
+	if( amount < 0 ) return 0;
 
-  else if( amount == 0 ) return 1;
+	else if( amount == 0 ) return 1;
 
-  else
-  {
-    int way0 = validSpending( amount-prices[0], prices, items ) ;
-    int way1 = validSpending( amount-prices[1], prices, items ) ;
-    int way2 = validSpending( amount-prices[2], prices, items ) ;
-    
-    return (way0+way1+way2) ;
-  }
+	else
+	{
+		int valid = 0;
+		for( int k=0; k<items; k++ )
+		{
+			valid = valid + validSpending( amount-prices[k], prices, items ) ;
+		}
+
+		return valid ;
+	}
 
 }
 
 int main()
 {
-    int ways = validSpending( amount, prices, items );
-    printf( "total valid ways of spending are : %d ", ways );
+	int ways = validSpending( amount, prices, items );
+	printf( "total valid ways of spending are : %d ", ways );
 }
